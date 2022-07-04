11881c2229289da15aaf4b764e05e7b188fb4075
Matt Habbo <matthabbo45@gmail.com>
2022-07-03 23:01:38 -0400

    savedbooks page currently not working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,9 +10,9 @@ const typeDefs = gql`
         title: String
     }
     type User {
-        _id: ID
-        username: String!
-        email: String!
+        _id: ID!
+        username: String
+        email: String
         bookCount: Int
         savedBooks: [Book]
     }
